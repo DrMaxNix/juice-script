860a58bbc274d1097e51a72e9977c4f963fcb332
DrMaxNix <contact@drmaxnix.de>
2022-10-02 17:06:34 +0200

    :tada: `div` command

---
+++ src/command/div.js
@@ -0,0 +1,48 @@
+Juicescript.command_define({
+	name: "div",
+	alias: [],
+	
+	validate: function(runner){
+		// count
+		runner.argument_validate_count({min: 2, max: null});
+		if(runner.has_error) return;
+		
+		// types
+		runner.argument_validate_type(1, Juicescript.argument_type.VARIABLE);
+		
+		for(var q = 2; q <= runner.command.argument.length; q++){
+			runner.argument_validate_type(q, Juicescript.argument_type.VALUE);
+		}
+	},
+	
+	execute: function(runner){
+		// GET VARIABLE //
+		// variable object
+		let variable = runner.argument_variable(1);
+		
+		// get its value
+		let value = runner.variable_get(variable);
+		
+		
+		// DEVIDE BY OTHER VALUES //
+		for(var q = 2; q <= runner.command.argument.length; q++){
+			// get value to devide by
+			let devide = runner.argument_value(q);
+			
+			// validate data type
+			let data_type = runner.data_type(devide);
+			if(data_type !== Juicescript.data_type.NUM){
+				// ignore with warning
+				runner.warning_argument(q, "invalid data type " + Juicescript.data_type.name(data_type));
+				continue;
+			}
+			
+			// do calculation
+			value /= devide;
+		}
+		
+		
+		// STORE BACK TO VARIABLE //
+		runner.variable_set(variable, value);
+	}
+});
