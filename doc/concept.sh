78ab4548da3157645073a294c3b5bf33c0f41722
DrMaxNix <contact@drmaxnix.de>
2022-10-05 21:03:21 +0200

    :memo: remove todo

--- doc/concept.sh
+++ doc/concept.sh
@@ -60,7 +60,7 @@ typ|type $1
 cst|cast $1 $2
 cst|cast $1 "num"
 
-# use contents of a var as var-name (todo: is there a more assembly-like way for this?)
+# use contents of a var as var-name
 ${$name}
 
 
