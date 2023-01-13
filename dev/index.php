45f9b0517164056dc09a6bc0b268b5a8bb4b0ba0
DrMaxNix <contact@drmaxnix.de>
2023-01-13 21:22:05 +0100

    :sparkles: on-demand light-mode

--- dev/index.php
+++ dev/index.php
@@ -7,6 +7,40 @@
 		
 		<meta name="viewport" content="width=device-width, initial-scale=0.8, user-scalable=0">
 		
+		<style media="screen">
+			:root {
+				<?php if(isset($_GET["light"])){ ?>
+					--onedark-bg: #fafafa;
+					--onedark-bg-light: #f0f0f0;
+					--onedark-white: #2c313a;
+					--onedark-gray: #686f7d;
+					--onedark-gray-dark: #8f96a3;
+					--onedark-gray-dark-dark: #abb1ba;
+					--onedark-red: #e45649;
+					--onedark-orange: #f79400;
+					--onedark-yellow: #f3c03f;
+					--onedark-green: #50a14f;
+					--onedark-cyan: #01b1bc;
+					--onedark-blue: #4078f2;
+					--onedark-purple: #a626a4;
+				<?php } else { ?>
+					--onedark-bg: #21252b;
+					--onedark-bg-light: #2c313a;
+					--onedark-white: #c5cad3;
+					--onedark-gray: #828997;
+					--onedark-gray-dark: #5c6370;
+					--onedark-gray-dark-dark: #454b54;
+					--onedark-red: #e06c75;
+					--onedark-orange: #d19a66;
+					--onedark-yellow: #e5c07b;
+					--onedark-green: #98c379;
+					--onedark-cyan: #56b6c2;
+					--onedark-blue: #61afef;
+					--onedark-purple: #c678dd;
+				<?php } ?>
+			}
+		</style>
+		
 		<link rel="stylesheet" href="/prism/prism-onedark.css">
 		<link rel="stylesheet" href="/prism/prism-line-numbers.css">
 		<link rel="stylesheet" href="/prism/prism-live.css">
